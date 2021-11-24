6ee110bf068cc3e56d8196f7cccba2bdf792a516
Claudiu HBann <123uidualcnnamreh@gmail.com>
2021-11-25 00:06:11 +0200

    done but not really

--- src/app/UConverter/convert-info/convert-info.component.ts
+++ src/app/UConverter/convert-info/convert-info.component.ts
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 
 export class ConvertInfoComponent implements OnInit {
 	public static categories = ["Currency", "Temperature", "Number.Base"]
-	public static categoriesTemperature = ["Celsius", "Fahrenheit", "Kelvin"]
+	public static categoriesFromTo: string[][] = [
+		["USD", "JPY", "BGN", "CZK", "DKK", "GBP", "HUF", "PLN", "RON", "SEK", "CHF", "ISK", "NOK", "HRK", "RUB", "TRY", "AUD", "BRL", "CAD", "CNY", "HKD", "IDR", "ILS", "INR", "KRW", "MXN", "MYR", "NZD", "PHP", "SGD", "THB", "ZAR"],
+		["Celsius", "Fahrenheit", "Kelvin"],
+		["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36"]
+	]
 
 	constructor() { }
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { ConvertInfoComponent } from './UConverter/convert-info/convert-info.component'
+import * as $ from 'jquery'
 
 @Component({
 	selector: 'app-root',
@@ -11,9 +12,16 @@ import { ConvertInfoComponent } from './UConverter/convert-info/convert-info.com
 export class AppComponent {
 	title = "Universal Converter"
 	currentCategory = "Choose a category..."
+	lastCategory1 = "Choose a category..."
+	lastCategory2 = "Choose a category..."
+
 	isFromInit = false
 	isToInit = false
 	index = 0
+
+	from = 0
+	to = 0
+
 	textAreaInputText = ""
 	textAreaOutputText = ""
 
@@ -35,18 +43,20 @@ export class AppComponent {
 	}
 
 	FromTo(index: number) {
-		if (index < ConvertInfoComponent.categoriesTemperature.length) {
+		if (index < ConvertInfoComponent.categoriesFromTo[ConvertInfoComponent.categories.indexOf(this.currentCategory.split(' ')[0])].length) {
 			var element = document.getElementById("dropdownMenuButtonFrom")
 
 			if (element) {
-				element.textContent = ConvertInfoComponent.categoriesTemperature[index]
+				element.textContent = ConvertInfoComponent.categoriesFromTo[ConvertInfoComponent.categories.indexOf(this.currentCategory.split(' ')[0])][index]
+				this.from = index
 			}
 		}
 		else {
 			var element = document.getElementById("dropdownMenuButtonTo")
 
 			if (element) {
-				element.textContent = ConvertInfoComponent.categoriesTemperature[index - ConvertInfoComponent.categoriesTemperature.length]
+				element.textContent = ConvertInfoComponent.categoriesFromTo[ConvertInfoComponent.categories.indexOf(this.currentCategory.split(' ')[0])][index - ConvertInfoComponent.categoriesFromTo[ConvertInfoComponent.categories.indexOf(this.currentCategory.split(' ')[0])].length]
+				this.to = index - ConvertInfoComponent.categoriesFromTo[ConvertInfoComponent.categories.indexOf(this.currentCategory.split(' ')[0])].length
 			}
 		}
 	}
@@ -73,41 +83,77 @@ export class AppComponent {
 	}
 
 	OnClickButtonFrom() {
+		if (this.lastCategory1 != this.currentCategory) {
+			var ul = document.getElementById('dropdownMenuFromUL')
+			if (ul) {
+				while (ul.firstChild) {
+					ul.removeChild(ul.firstChild)
+				}
+			}
+
+			this.isFromInit = false
+		}
+
 		if (this.isFromInit == false) {
-			ConvertInfoComponent.categoriesTemperature.forEach(element => {
+			ConvertInfoComponent.categoriesFromTo[ConvertInfoComponent.categories.indexOf(this.currentCategory.split(' ')[0])].forEach(element => {
 				this.AppendLIWithAToUL("dropdownMenuFromUL", '#', "dropdown-item", element, this.index++)
 			})
 
 			this.isFromInit = true
+			this.lastCategory1 = this.currentCategory
 		}
 	}
 
 	OnClickButtonTo() {
+		if (this.lastCategory2 != this.currentCategory) {
+			var ul = document.getElementById('dropdownMenuToUL')
+			if (ul) {
+				while (ul.firstChild) {
+					ul.removeChild(ul.firstChild)
+				}
+			}
+
+			this.isToInit = false
+		}
+
 		if (this.isToInit == false) {
-			ConvertInfoComponent.categoriesTemperature.forEach(element => {
+			ConvertInfoComponent.categoriesFromTo[ConvertInfoComponent.categories.indexOf(this.currentCategory.split(' ')[0])].forEach(element => {
 				this.AppendLIWithAToUL("dropdownMenuToUL", '#', "dropdown-item", element, this.index++)
 			})
 
 			this.isToInit = true
+			this.lastCategory2 = this.currentCategory
 		}
 	}
 
+	replaceAll(str: string, find: string, replace: string) {
+		return str.replace(new RegExp(find, 'g'), replace);
+	  }
+
 	OnClickButtonConvert() {
+		let items = new Array()
+
+		this.textAreaInputText.split(',').forEach(element => {
+			element = this.replaceAll(element, " ", "")
+			element = this.replaceAll(element, ",", "")
+			element = this.replaceAll(element, "\n", "")
+			items.push(element)
+		})
+
+		console.log(items)
+
 		this.http.post<any>("https://localhost:7212/ConvertInfo/Post", {
-			"category": 1,
-			"items": [
-				"string1",
-				"string2",
-				"string3",
-				"string3"
-			],
-			"from": 2,
-			"to": 3
+			"category": ConvertInfoComponent.categories.indexOf(this.currentCategory.split(' ')[0]),
+			"items": JSON.stringify(items),
+			"from": this.from,
+			"to": this.to
 		}, {
 			headers: { "Content-Type": "application/json" }
 		}).subscribe({
 			next: result => {
-				console.log(result)
+				result.forEach((item: string) => {
+					this.textAreaOutputText += item + '\n'
+				})
 			},
 			error: error => {
 				console.error(error)
@@ -117,5 +163,17 @@ export class AppComponent {
 
 	OnClickButtonCTC() {
 		navigator.clipboard.writeText(this.textAreaOutputText)
+
+		var x = document.getElementById("toastID");
+
+		if (x != null) {
+			x.className = "show";
+
+			setTimeout(function () {
+				if (x != null) {
+					x.className = x.className.replace("show", "");
+				}
+			}, 3000);
+		}
 	}
 }
